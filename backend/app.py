ba92991062ce5f872b0f996eadc17baf47143db3
cbm94 <cbm94@cornell.edu>
2023-05-01 23:55:05 -0400

    test

--- backend/app.py
+++ backend/app.py
@@ -22,8 +22,7 @@ MYSQL_USER_PASSWORD = "a0htd_dys"
 MYSQL_PORT = 4540
 MYSQL_DATABASE = "bookbeatsdb"
 
-mysql_engine = MySQLDatabaseHandler(
-    MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
+mysql_engine = MySQLDatabaseHandler(MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
 mysql_engine.load_file_into_db()
